08daa23d2bf0a24c5de2570a818c1f6bc7bf86b2
Samyrsouza244 <samyr1479ee@gmail.com>
2022-07-12 11:42:59 -0300

    desafio entregue

---
+++ script.js
@@ -0,0 +1,12 @@
+let name ="Samyr";
+let number = 13;
+
+let year = 2022;
+let Age = year - number;{
+    console.log(year-number);
+}
+
+let day='quartaFeira';
+if('true'){
+    console.log('quartaFeira')
+}
